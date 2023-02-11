72b097bd66b8abfe626ea09d1f5b1474efe9883e
Victor M. Rivas <vrivas@ujaen.es>
2023-02-11 07:43:49 +0100

    Añado campos editables al formulario de persona

--- static-files/js/personas.js
+++ static-files/js/personas.js
@@ -9,6 +9,13 @@
 /// Creo el espacio de nombres
 let Personas = {};
 
+/// Nombre de los campos del formulario para editar una persona
+Personas.form = {
+    NOMBRE: "form-persona-nombre",
+    APELLIDOS: "form-persona-apellidos",
+    EMAIL: "form-persona-email",
+    ANIO: "form-persona-anio",
+}
 
 /**
  * Función que recuperar todas las personas llamando al MS Personas
@@ -16,21 +23,21 @@ let Personas = {};
  */
 
 Personas.recupera = async function (callBackFn) {
-    let response=null
+    let response = null
 
     // Intento conectar con el microservicio personas
     try {
         const url = Frontend.API_GATEWAY + "/personas/getTodas"
         response = await fetch(url)
-       
+
     } catch (error) {
-        alert("Error: No se han podido acceder al API Gateway" )
+        alert("Error: No se han podido acceder al API Gateway")
         console.error(error)
         //throw error
     }
 
     // Muestro todas las persoans que se han descargado
-    let vectorPersonas=null
+    let vectorPersonas = null
     if (response) {
         vectorPersonas = await response.json()
         callBackFn(vectorPersonas.data)
@@ -52,7 +59,7 @@ Personas.recuperaUnaPersona = async function (idPersona, callBackFn) {
             callBackFn(persona)
         }
     } catch (error) {
-        alert("Error: No se han podido acceder al API Gateway" )
+        alert("Error: No se han podido acceder al API Gateway")
         console.error(error)
     }
 }
@@ -66,7 +73,7 @@ Personas.recuperaUnaPersona = async function (idPersona, callBackFn) {
 Personas.cabeceraTABLE = function () {
     return `<table class="listado-personas">
         <thead>
-        <th>Nombre</th><th>Apellidos</th><th>eMail</th><th>Año contratación</th>
+        <th>Id</th><th>Nombre</th><th>Apellidos</th><th>eMail</th><th>Año contratación</th><th>Acciones</th>
         </thead>
         <tbody>
     `;
@@ -79,10 +86,16 @@ Personas.cabeceraTABLE = function () {
  */
 Personas.cuerpoTr = function (p) {
     return `<tr title="${p.ref['@ref'].id}">
-    <td>${p.data.nombre}</td>
-    <td>${p.data.apellidos}</td>
-    <td>${p.data.email}</td>
-    <td>${p.data.año_entrada}</td>
+    <td><input type="text" class="form-persona-elemento" disabled id="form-persona-id" value="${p.ref['@ref'].id}"/></td>
+    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-nombre" required value="${p.data.nombre}"/></td>
+    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-apellidos"  value="${p.data.apellidos}"/></td>
+    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-email"  required value="${p.data.email}"/></td>
+    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-anio"  required value="${p.data.año_entrada}"/></td>
+    <td>
+        <div class="opcion-secundaria"><a href="javascript:Personas.editar()">Editar</a></div>
+        <div class="opcion-terciaria editar"><a href="javascript:Personas.guardar()">Guardar</a></div>
+        <div class="opcion-terciaria editar"><a href="javascript:Personas.cancelar()">Cancelar</a></div>
+    </td>
     </tr>
     `;
 }
@@ -110,7 +123,7 @@ Personas.imprime = function (vector) {
     msj += Personas.pieTable()
 
     // Borro toda la info de Article y la sustituyo por la que me interesa
-    Frontend.Article.actualizar( "Listado de personas", msj )
+    Frontend.Article.actualizar("Listado de personas", msj)
 }
 
 /**
@@ -125,7 +138,7 @@ Personas.imprimeUnaPersona = function (persona) {
     msj += Personas.cuerpoTr(persona);
     msj += Personas.pieTable();
     // Borro toda la info de Article y la sustituyo por la que me interesa
-    Frontend.Article.actualizar( "Mostrar una persona", msj )
+    Frontend.Article.actualizar("Mostrar una persona", msj)
 }
 
 
@@ -140,8 +153,56 @@ Personas.listar = function () {
 /**
  * Función principal para mostrar los datos de una persona desde el MS y, posteriormente, imprimirla.
  * @param {String} idPersona Identificador de la persona a mostrar
- * @returns True
  */
 Personas.mostrar = function (idPersona) {
     this.recuperaUnaPersona(idPersona, this.imprimeUnaPersona);
+}
+
+
+/**
+ * Establece disable = false en los campos editables
+ * @returns El propio objeto Personas, para concatenar llamadas
+ */
+Personas.habilitarCamposEditables = function () {
+    for (let campo in Personas.form) {
+        document.getElementById(Personas.form[campo]).disabled = false
+    }
+    return this
+}
+
+
+/**
+ * Oculta todas las opciones secundarias
+ * @returns El propio objeto para encadenar llamadas
+ */
+Personas.ocultarOpcionesSecundarias = function () {
+    let opciones = document.getElementsByClassName("opcion-secundaria")
+    for (let i = 0; i < opciones.length; ++i) {
+        Frontend.quitarClase(opciones[i], Frontend.CLASS_MOSTRAR)
+            .aniadirClase(opciones[i], Frontend.CLASS_OCULTAR)
+    }
+    return this
+}
+
+/**
+ * Muestra las opciones que tiene el usuario cuando selecciona Editar
+ * @returns El propio objeto Personas, para concatenar llamadas
+ */
+Personas.mostrarOcionesTerciariasEditar = function () {
+    Personas.ocultarOpcionesSecundarias()
+    let opciones = document.getElementsByClassName("opcion-terciaria editar")
+    for (let i = 0; i < opciones.length; ++i) {
+        Frontend.quitarClase(opciones[i], Frontend.CLASS_OCULTAR)
+            .aniadirClase(opciones[i], Frontend.CLASS_MOSTRAR)
+    }
+    return this
+}
+
+
+/**
+ * Función que permite modificar los datos de una persona
+ */
+Personas.editar = function () {
+    this.mostrarOcionesTerciariasEditar()
+    this.habilitarCamposEditables()
 }
