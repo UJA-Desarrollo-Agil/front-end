dee49fc32f2fb7106715ae461b72b2f811b17f5d
Victor M. Rivas <vrivas@ujaen.es>
2023-02-11 16:44:33 +0100

    Convierto en plantilla la tabla que muestra una persona

--- static-files/js/personas.js
+++ static-files/js/personas.js
@@ -19,7 +19,68 @@ Personas.form = {
 }
 
 /// Objeto para almacenar los datos de la persona que se está mostrando
-Personas.personaMostrada=null
+Personas.personaMostrada = null
+
+/// Plantilla para poner los datos de una persona en un TR
+Personas.plantillaFormularioPersona = {}
+Personas.plantillaFormularioPersona.tags = {
+    "ID": "### ID ###",
+    "NOMBRE": "### NOMBRE ###",
+    "APELLIDOS": "### APELLIDOS ###",
+    "EMAIL": "### EMAIL ###",
+    "AÑO ENTRADA": "### AÑO ENTRADA ###",
+}
+Personas.plantillaFormularioPersona.cabecera = `<table class="listado-personas">
+                    <thead>
+                        <th width="5%">Id</th>
+                        <th width="15%">Nombre</th>
+                        <th width="15%">Apellidos</th>
+                        <th width="10%">eMail</th>
+                        <th width="5%">Año contratación</th>
+                        <th>Acciones</th>
+                    </thead>
+                    <tbody>
+    `;
+Personas.plantillaFormularioPersona.cuerpo = `         <tr title="${Personas.plantillaFormularioPersona.tags.ID}">
+                            <td><input type="text" class="form-persona-elemento" disabled id="form-persona-id"
+                                    value="${Personas.plantillaFormularioPersona.tags.ID}" /></td>
+                            <td><input type="text" class="form-persona-elemento editable" disabled
+                                    id="form-persona-nombre" required value="${Personas.plantillaFormularioPersona.tags.NOMBRE}" /></td>
+                            <td><input type="text" class="form-persona-elemento editable" disabled
+                                    id="form-persona-apellidos" value="### APELLIDOS ###" /></td>
+                            <td><input type="email" class="form-persona-elemento editable" disabled
+                                    id="form-persona-email" required value="### EMAIL ###" /></td>
+                            <td><input type="number" class="form-persona-elemento editable" disabled
+                                    id="form-persona-anio" min="1950" max="2030" required
+                                    value="### AÑO ENTRADA ###" /></td>
+                            <td>
+                                <div class="opcion-secundaria"><a href="javascript:Personas.editar()">Editar</a></div>
+                                <div class="opcion-terciaria editar"><a href="javascript:Personas.guardar()">Guardar</a>
+                                </div>
+                                <div class="opcion-terciaria editar"><a
+                                        href="javascript:Personas.cancelar()">Cancelar</a></div>
+                            </td>
+                        </tr>
+    `;
+Personas.plantillaFormularioPersona.pie = `        </tbody>
+             </table>
+             `;
+
+/**
+     * Actualiza el cuerpo de la plantilla con los datos de la persona que se le pasa
+     * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
+     * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
+     */
+Personas.plantillaFormularioPersona.actualiza = function (persona) {
+    // Utilizo expresiones regulares con el modificador 'g' para que cambie todas las apariciones de cada tag
+    return this.cuerpo
+        .replace(new RegExp(this.tags.ID, 'g'), persona.ref['@ref'].id)
+        .replace(new RegExp(this.tags.NOMBRE, 'g'), persona.data.nombre)
+        .replace(new RegExp(this.tags.APELLIDOS, 'g'), persona.data.apellidos)
+        .replace(new RegExp(this.tags.EMAIL, 'g'), persona.data.email)
+        .replace(new RegExp(this.tags["AÑO ENTRADA"], 'g'), persona.data.año_entrada)
+}
+
 
 /**
  * Función que recuperar todas las personas llamando al MS Personas
@@ -75,9 +136,14 @@ Personas.recuperaUnaPersona = async function (idPersona, callBackFn) {
  * @returns Cabecera de la tabla
  */
 Personas.cabeceraTABLE = function () {
-    return `<table class="listado-personas">
+    return `< table class="listado-personas" >
         <thead>
-        <th>Id</th><th>Nombre</th><th>Apellidos</th><th>eMail</th><th>Año contratación</th><th>Acciones</th>
+        <th width="5%">Id</th>
+        <th width="15%">Nombre</th>
+        <th width="15%">Apellidos</th>
+        <th width="10%">eMail</th>
+        <th width="5%">Año contratación</th>
+        <th>Acciones</th>
         </thead>
         <tbody>
     `;
@@ -86,24 +152,24 @@ Personas.cabeceraTABLE = function () {
 /**
  * Muestra la información de cada persona en un elemento TR con sus correspondientes TD
  * @param {persona} p Datos de la persona a mostrar
- * @returns Cadena conteniendo todo el elemento TR que muestra la persona.
- */
+        * @returns Cadena conteniendo todo el elemento TR que muestra la persona.
+        */
 Personas.cuerpoTr = function (p) {
     return `<tr title="${p.ref['@ref'].id}">
-    <td><input type="text" class="form-persona-elemento" disabled id="form-persona-id" value="${p.ref['@ref'].id}"/></td>
-    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-nombre" required value="${p.data.nombre}"/></td>
-    <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-apellidos"  value="${p.data.apellidos}"/></td>
-    <td><input type="email" class="form-persona-elemento editable" disabled id="form-persona-email"  required value="${p.data.email}"/></td>
-    <td><input type="number" class="form-persona-elemento editable" disabled id="form-persona-anio"  
-        min="1950" max="2030"
-        required value="${p.data.año_entrada}"/></td>
-    <td>
-        <div class="opcion-secundaria"><a href="javascript:Personas.editar()">Editar</a></div>
-        <div class="opcion-terciaria editar"><a href="javascript:Personas.guardar()">Guardar</a></div>
-        <div class="opcion-terciaria editar"><a href="javascript:Personas.cancelar()">Cancelar</a></div>
-    </td>
-    </tr>
-    `;
+            <td><input type="text" class="form-persona-elemento" disabled id="form-persona-id" value="${p.ref['@ref'].id}" /></td>
+            <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-nombre" required value="${p.data.nombre}" /></td>
+            <td><input type="text" class="form-persona-elemento editable" disabled id="form-persona-apellidos" value="${p.data.apellidos}" /></td>
+            <td><input type="email" class="form-persona-elemento editable" disabled id="form-persona-email" required value="${p.data.email}" /></td>
+            <td><input type="number" class="form-persona-elemento editable" disabled id="form-persona-anio"
+                min="1950" max="2030"
+                required value="${p.data.año_entrada}" /></td>
+            <td>
+                <div class="opcion-secundaria"><a href="javascript:Personas.editar()">Editar</a></div>
+                <div class="opcion-terciaria editar"><a href="javascript:Personas.guardar()">Guardar</a></div>
+                <div class="opcion-terciaria editar"><a href="javascript:Personas.cancelar()">Cancelar</a></div>
+            </td>
+        </tr>
+        `;
 }
 
 /**
@@ -111,8 +177,21 @@ Personas.cuerpoTr = function (p) {
  * @returns Cadena con el pie de la tabla
  */
 Personas.pieTable = function () {
-    return "</tbody></table>";
+    return "</tbody></table > ";
 }
+
+
+/**
+ * Imprime los datos de una persona como una tabla usando la plantilla del formulario.
+ * @param {persona} Persona Objeto con los datos de la persona
+ * @returns Una cadena con la tabla que tiene ya los datos actualizados
+ */
+Personas.personaComoTable = function (persona) {
+    return Personas.plantillaFormularioPersona.cabecera
+        + Personas.plantillaFormularioPersona.actualiza(persona)
+        + Personas.plantillaFormularioPersona.pie;
+}
+
 
 /**
  * Función para mostrar en pantalla todas las personas que se han recuperado de la BBDD.
@@ -123,10 +202,9 @@ Personas.imprime = function (vector) {
     // console.log(vector) // Para comprobar lo que hay en vector
 
     // Compongo el contenido que se va a mostrar dentro de la tabla
-    let msj = ""
-    msj += Personas.cabeceraTABLE()
-    vector.forEach(e => msj += Personas.cuerpoTr(e))
-    msj += Personas.pieTable()
+    let msj = Personas.plantillaFormularioPersona.cabecera
+    vector.forEach(e => msj += Personas.plantillaFormularioPersona.actualiza(e))
+    msj += Personas.plantillaFormularioPersona.pie
 
     // Borro toda la info de Article y la sustituyo por la que me interesa
     Frontend.Article.actualizar("Listado de personas", msj)
@@ -139,15 +217,13 @@ Personas.imprime = function (vector) {
 
 Personas.imprimeUnaPersona = function (persona) {
     // console.log(persona) // Para comprobar lo que hay en vector
-    let msj = "";
-    msj += Personas.cabeceraTABLE();
-    msj += Personas.cuerpoTr(persona);
-    msj += Personas.pieTable();
+    let msj = Personas.personaComoTable(persona);
+
     // Borro toda la info de Article y la sustituyo por la que me interesa
     Frontend.Article.actualizar("Mostrar una persona", msj)
 
     // Actualiza el objeto que guarda los datos mostrados
-    Personas.almacenaDatos( persona )
+    Personas.almacenaDatos(persona)
 }
 
 /**
@@ -156,7 +232,7 @@ Personas.imprimeUnaPersona = function (persona) {
  */
 
 Personas.almacenaDatos = function (persona) {
-    Personas.personaMostrada=persona;
+    Personas.personaMostrada = persona;
 }
 
 /**
@@ -190,8 +266,8 @@ Personas.mostrar = function (idPersona) {
  * @param {boolean} Deshabilitando Indica si queremos deshabilitar o habilitar los campos
  * @returns El propio objeto Personas, para concatenar llamadas
  */
-Personas.habilitarDeshabilitarCamposEditables = function ( deshabilitando ) {
-    deshabilitando = (typeof deshabilitando==="undefined" || deshabilitando===null)?true:deshabilitando
+Personas.habilitarDeshabilitarCamposEditables = function (deshabilitando) {
+    deshabilitando = (typeof deshabilitando === "undefined" || deshabilitando === null) ? true : deshabilitando
     for (let campo in Personas.form) {
         document.getElementById(Personas.form[campo]).disabled = deshabilitando
     }
@@ -203,8 +279,8 @@ Personas.habilitarDeshabilitarCamposEditables = function ( deshabilitando ) {
  * Establece disable = true en los campos editables
  * @returns El propio objeto Personas, para concatenar llamadas
  */
-Personas.deshabilitarCamposEditables = function ( ) {
-    Personas.habilitarDeshabilitarCamposEditables( true )
+Personas.deshabilitarCamposEditables = function () {
+    Personas.habilitarDeshabilitarCamposEditables(true)
     return this
 }
 
@@ -213,8 +289,8 @@ Personas.deshabilitarCamposEditables = function ( ) {
  * Establece disable = false en los campos editables
  * @returns El propio objeto Personas, para concatenar llamadas
  */
-Personas.habilitarCamposEditables = function ( ) {
-    Personas.habilitarDeshabilitarCamposEditables( false )
+Personas.habilitarCamposEditables = function () {
+    Personas.habilitarDeshabilitarCamposEditables(false)
     return this
 }
 
@@ -223,11 +299,11 @@ Personas.habilitarCamposEditables = function ( ) {
  * ????Muestra las opciones que tiene el usuario cuando selecciona Editar
  * @returns El propio objeto Personas, para concatenar llamadas
  */
-Personas.opcionesMostrarOcultar = function ( classname, mostrando ) {
+Personas.opcionesMostrarOcultar = function (classname, mostrando) {
     let opciones = document.getElementsByClassName(classname)
-    let claseQuitar=mostrando?Frontend.CLASS_OCULTAR:Frontend.CLASS_MOSTRAR
-    let claseAniadir=!mostrando?Frontend.CLASS_OCULTAR:Frontend.CLASS_MOSTRAR
-    
+    let claseQuitar = mostrando ? Frontend.CLASS_OCULTAR : Frontend.CLASS_MOSTRAR
+    let claseAniadir = !mostrando ? Frontend.CLASS_OCULTAR : Frontend.CLASS_MOSTRAR
+
     for (let i = 0; i < opciones.length; ++i) {
         Frontend.quitarClase(opciones[i], claseQuitar)
             .aniadirClase(opciones[i], claseAniadir)
@@ -240,7 +316,7 @@ Personas.opcionesMostrarOcultar = function ( classname, mostrando ) {
  * @returns El propio objeto para encadenar llamadas
  */
 Personas.ocultarOpcionesSecundarias = function () {
-    this.opcionesMostrarOcultar("opcion-secundaria", false )
+    this.opcionesMostrarOcultar("opcion-secundaria", false)
     return this
 }
 
@@ -250,7 +326,7 @@ Personas.ocultarOpcionesSecundarias = function () {
  * @returns El propio objeto para encadenar llamadas
  */
 Personas.mostrarOpcionesSecundarias = function () {
-    this.opcionesMostrarOcultar("opcion-secundaria", true )
+    this.opcionesMostrarOcultar("opcion-secundaria", true)
     return this
 }
 
@@ -288,9 +364,12 @@ Personas.editar = function () {
  * Función que permite cancelar la acción sobre los datos de una persona
  */
 Personas.cancelar = function () {
-    this.imprimeUnaPersona( this.recuperaDatosAlmacenados() )
+    this.imprimeUnaPersona(this.recuperaDatosAlmacenados())
     this.deshabilitarCamposEditables()
     this.ocultarOcionesTerciariasEditar()
     this.mostrarOpcionesSecundarias()
-    
+
 }
+
+
+
